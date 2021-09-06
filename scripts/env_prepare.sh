edf5f943c880f62dd3032d261df96ef4721babd1
Victor Cassen <phonybone@gmail.com>
2021-09-05 17:33:46 -0700

    env_prepare.sh

--- scripts/env_prepare.sh
+++ scripts/env_prepare.sh
@@ -1,1 +1,4 @@
-export SQLALCHEMY_DB_URI='postgresql://postgres:this-is-a-very-long-password-ducky@localhost/account'
+export APP_ROOT=account_api
+export FLASK_ENV=Development
+export SQLALCHEMY_DATABASE_URI='postgresql://postgres:this-is-a-very-long-password-ducky@localhost/account'
+export FLASK_APP=${APP_ROOT}.app
