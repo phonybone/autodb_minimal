4fe0ac0c3f48973d6e6dc1b91b37a8bee7f5fafa
Victor Cassen <phonybone@gmail.com>
2021-09-05 17:51:12 -0700

    config.py: cleanup dead code

--- account_api/config.py
+++ account_api/config.py
@@ -10,7 +10,6 @@ class Config:
     DEBUG = True
     TESTING = False
 
-    # import ipdb; ipdb.set_trace()
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -33,7 +32,3 @@ class Testing(Config):
     Testing config
     """
     pass
-    # SERVER_NAME = 'localhost'
-    # TESTING = True
-    # SQLALCHEMY_DATABASE_URI = F'sqlite:///{root}/autodb/test.db'
-    # # SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.environ["HOME"]}/Dropbox/sandbox/python/flasks/autodb/test.db'
